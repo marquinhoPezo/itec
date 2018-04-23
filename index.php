0365b698fc3b8c1d75296eeeb684128ff8dacc60
= <marq_253@hotmail.com>
2018-04-22 23:41:29 -0500

    Avanze html casi completo

--- index.php
+++ index.php
@@ -1,10 +1,88 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Itec</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
-	
+	<div id="agrupar">
+		<header id="cabecera">
+			<h1>Este es el titulo principal del sitio web </h1>
+		</header>
+		<nav id="menu">
+			<ul>
+				<li>Principal</li>
+				<li>Fotos</li>
+				<li>Videos</li>
+				<li>Contactos</li>
+			</ul>
+		</nav>
+		<section id="seccion">
+			<article>
+				<header>
+					<hgroup>
+						<h1>Titulo del Mensaje Uno</h1>
+						<h2>Subtitlo del Mensaje Uno</h2>
+					</hgroup>
+					<time datatime="2011-12-10" pubdate>publicado 05-11-2013</time>
+				</header>
+			
+			Este es el texto de mi primer mensaje.
+		
+				<figure>
+					<img src="http://www.minkbooks.com/images/coveriosbigen.png">
+					<figcaption>
+					Esta es la imagen del primer mensaje.
+					</figcaption>
+				</figure>
+				<footer>
+					Comentarios(O)
+				</footer>
+			</article>
+			<article>
+				<header>
+					<hgroup>
+						<h1>Titulo de mensaje 2</h1>
+						<h2>Subtitlo de mensaje 2</h2>
+					</hgroup>
+					<time datatime="2011-12-15" pubdate>publicado 05-11-2013</time>
+				</header>
+			</article>
+			<article>
+				<!-- Segunda parte 	-->
+				<article>
+				<header>
+					<hgroup>
+						<h1>Titulo del Mensaje Uno</h1>
+						<h2>Subtitlo del Mensaje Uno</h2>
+					</hgroup>
+					<time datatime="2011-12-10" pubdate>publicado 05-11-2013</time>
+				</header>
+				Este es el texto de mi primer mensaje.
+				<footer>
+					Comentarios(O)
+				</footer>
+			</article>
+			<article>
+				<header>
+					<hgroup>
+						<h1>Titulo de mensaje 2</h1>
+						<h2>Subtitlo de mensaje 2</h2>
+					</hgroup>
+					<time datatime="2011-12-15" pubdate>publicado 05-11-2013</time>
+				</header>
+			</article>
+			</article>
+				<!-- Fin de segunda parte  -->
+		</section>
+		<aside id="columna">
+			<blockquote>Mensaje numero uno</blockquote>
+			<blockquote>Mensaje numero dos</blockquote>
+		</aside>
+		<footer id="pie">
+			&copy;  
+		</footer>
+	</div>
 </body>
 </html>
