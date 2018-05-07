341151dd7e59564d5de965b47dd3cd45236541d4
= <marq_253@hotmail.com>
2018-05-07 00:05:22 -0500

    Inicio/Finalizacion video 3 - Por empezar el video 4

---
+++ plantilla.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Estilos con CSS3</title>
+	<link rel="stylesheet" type="text/css" href="css/estilosp.css">
+</head>
+<body>
+	<header id="principal">
+		<span id="titulo">Plantillas con  Estilos CSS3</span>
+	</header>
+</body>
+</html>
